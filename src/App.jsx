19b240c1bc48c930e60fe804471a9d07b268b443
Govind <govindkarn777@gmail.com>
2025-04-29 13:48:12 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Button}  from "../src/components/ui/button"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Button>hellllo</Button>
+    </>
+  )
+}
+
+export default App
